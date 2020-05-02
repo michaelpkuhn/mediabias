a5c4f10a047479aab1c940856d2451337a031ed8
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-02 14:42:42 -0500

    Added classifier to predict if tweet is by R or D

---
+++ h_dev/test_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+from joblib import load
+# Tools to remove stopwords from tweets
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+from sklearn.linear_model import SGDClassifier
+#from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('index.html',party_prediction = '')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    def list_tostring(input_list):
+        return ' '.join(input_list)
+    in_text = request.form['text']
+    fun_input = list_tostring(word_tokenize(in_text))
+    bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
+    bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
+    X_pred = bigram_vectorizer.transform([fun_input])
+    X_pred = bigram_tf_idf_transformer.transform(X_pred)
+    sgd_classifier = load('classifiers/sgd_classifier.joblib')
+    result = sgd_classifier.predict(X_pred)
+    if result[0] == 'R':
+        party_result = 'Predicted Republican Tweet'
+    else:
+        party_result = 'Predicted Democrat Tweet'
+    return render_template('index.html', party_prediction = party_result)
+
+app.debug = True
+app.run()
