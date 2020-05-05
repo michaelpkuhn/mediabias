f387868567eb6e328f0d67f1367c40c2a35987cf
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-05 00:19:10 -0500

    uncommented section

--- webpage/app.py
+++ webpage/app.py
@@ -9,14 +9,14 @@ nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 
-# from sklearn.linear_model import SGDClassifier
-# from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-# stop_words = set(stopwords.words('english'))
+stop_words = set(stopwords.words('english'))
 
-# bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
-# bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
-# sgd_classifier = load('classifiers/sgd_classifier.joblib')
+bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
+bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
+sgd_classifier = load('classifiers/sgd_classifier.joblib')
 
 
 app = Flask(__name__)
@@ -45,21 +45,20 @@ def my_form():
 
 @app.route('/mlmodels', methods=['POST'])
 def mlmodels():
-    # def list_tostring(input_list):
-    #     return ' '.join(input_list)
-    # def remove_stopwords(input_list):
-    #     return [w for w in input_list if not w in stop_words]
-    # in_text = request.form['text']
-    # fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
+    def list_tostring(input_list):
+        return ' '.join(input_list)
+    def remove_stopwords(input_list):
+        return [w for w in input_list if not w in stop_words]
+    in_text = request.form['text']
+    fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
 
-    # X_pred = bigram_vectorizer.transform([fun_input])
-    # X_pred = bigram_tf_idf_transformer.transform(X_pred)
-    # result = sgd_classifier.predict(X_pred)
-    # if result[0] == 'R':
-    #     party_result = 'Predicted Republican Tweet'
-    # else:
-    #     party_result = 'Predicted Democrat Tweet'
-    party_result =  request.form['text']
+    X_pred = bigram_vectorizer.transform([fun_input])
+    X_pred = bigram_tf_idf_transformer.transform(X_pred)
+    result = sgd_classifier.predict(X_pred)
+    if result[0] == 'R':
+        party_result = 'Predicted Republican Tweet'
+    else:
+        party_result = 'Predicted Democrat Tweet'
     return render_template('mlmodels.html', party_prediction = party_result)
 
 @app.route('/sitemap')
