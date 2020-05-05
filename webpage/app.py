29bda1a744a171d2d7aa66a5bcadb8755368a2e8
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-05 10:10:11 -0500

    debugging

--- webpage/app.py
+++ webpage/app.py
@@ -14,9 +14,10 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 stop_words = set(stopwords.words('english'))
 
-bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
-bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
-sgd_classifier = load('classifiers/sgd_classifier.joblib')
+# bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
+# bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
+temp_path = os.path.abspath('classifiers/sgd_classifier.joblib')
+sgd_classifier = load(temp_path)
 
 app = Flask(__name__)
 app.config['DEBUG']= True
@@ -51,13 +52,15 @@ def mlmodels():
         return [w for w in input_list if not w in stop_words]
     fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
 
-    X_pred = bigram_vectorizer.transform([fun_input])
-    X_pred = bigram_tf_idf_transformer.transform(X_pred)
-    result = sgd_classifier.predict(X_pred)
+    #X_pred = bigram_vectorizer.transform([fun_input])
+    #X_pred = bigram_tf_idf_transformer.transform(X_pred)
+    #result = sgd_classifier.predict(X_pred)
+    result = 'pizza'
     if result[0] == 'R':
         party_result = 'Predicted Republican Tweet'
     else:
         party_result = 'Predicted Democrat Tweet'
+        party_result = 'Repeated text: '+in_text+' '+temp_path
     return render_template('mlmodels.html', party_prediction = party_result)
 
 @app.route('/sitemap')
