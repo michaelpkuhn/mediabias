0995aee24e8bc46d8766089470e9f291d0212bbb
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-04 23:31:52 -0500

    temp commit

--- webpage/app.py
+++ webpage/app.py
@@ -43,23 +43,23 @@ def method():
 def my_form():
     return render_template('mlmodels.html',party_prediction = '')
 
-@app.route('/mlmodels', methods=['POST'])
-def mlmodels():
-    def list_tostring(input_list):
-        return ' '.join(input_list)
-    def remove_stopwords(input_list):
-        return [w for w in input_list if not w in stop_words]
-    in_text = request.form['text']
-    fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
+# @app.route('/mlmodels', methods=['POST'])
+# def mlmodels():
+#     def list_tostring(input_list):
+#         return ' '.join(input_list)
+#     def remove_stopwords(input_list):
+#         return [w for w in input_list if not w in stop_words]
+#     in_text = request.form['text']
+#     fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
 
-    X_pred = bigram_vectorizer.transform([fun_input])
-    X_pred = bigram_tf_idf_transformer.transform(X_pred)
-    result = sgd_classifier.predict(X_pred)
-    if result[0] == 'R':
-        party_result = 'Predicted Republican Tweet'
-    else:
-        party_result = 'Predicted Democrat Tweet'
-    return render_template('mlmodels.html', party_prediction = party_result)
+#     X_pred = bigram_vectorizer.transform([fun_input])
+#     X_pred = bigram_tf_idf_transformer.transform(X_pred)
+#     result = sgd_classifier.predict(X_pred)
+#     if result[0] == 'R':
+#         party_result = 'Predicted Republican Tweet'
+#     else:
+#         party_result = 'Predicted Democrat Tweet'
+#     return render_template('mlmodels.html', party_prediction = party_result)
 
 @app.route('/sitemap')
 def sitemap():
