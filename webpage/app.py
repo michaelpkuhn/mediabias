f601d9285d774d9e137c5d143f96584392d2084c
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-04 22:24:53 -0500

    added sgd classifier model

--- webpage/app.py
+++ webpage/app.py
@@ -10,7 +10,8 @@ nltk.download('punkt')
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-stop_words = set(stopwords.words('english'))
+
+
 
 app = Flask(__name__)
 app.config['DEBUG']= True
@@ -32,9 +33,27 @@ def classify():
 def method():
     return render_template('Methodology.html')
 
-@app.route('/mlmodels')
+@app.route('/mlmodels', methods=['POST'])
+stop_words = set(stopwords.words('english'))
+bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
+bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
+sgd_classifier = load('classifiers/sgd_classifier.joblib')
 def mlmodels():
-    return render_template('mlmodels.html')
+    def list_tostring(input_list):
+        return ' '.join(input_list)
+    def remove_stopwords(input_list):
+        return [w for w in input_list if not w in stop_words]
+    in_text = request.form['text']
+    fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
+
+    X_pred = bigram_vectorizer.transform([fun_input])
+    X_pred = bigram_tf_idf_transformer.transform(X_pred)
+    result = sgd_classifier.predict(X_pred)
+    if result[0] == 'R':
+        party_result = 'Predicted Republican Tweet'
+    else:
+        party_result = 'Predicted Democrat Tweet'
+    return render_template('index.html', party_prediction = party_result)
 
 @app.route('/sitemap')
 def sitemap():
