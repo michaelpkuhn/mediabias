69d5db2f08c76748760fec96c93de1df788acdfa
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-05 00:46:39 -0500

    debugging

--- webpage/app.py
+++ webpage/app.py
@@ -57,11 +57,12 @@ def mlmodels():
     #X_pred = bigram_vectorizer.transform([fun_input])
     #X_pred = bigram_tf_idf_transformer.transform(X_pred)
     #result = sgd_classifier.predict(X_pred)
-    result = 'pizza'
-    if result[0] == 'R':
-        party_result = 'Predicted Republican Tweet'
-    else:
-        party_result = 'Predicted Democrat Tweet'+'pizza'
+    # result = 'pizza'
+    # if result[0] == 'R':
+    #     party_result = 'Predicted Republican Tweet'
+    # else:
+    #     party_result = 'Predicted Democrat Tweet'+'pizza'
+    party_result = in_text
     return render_template('mlmodels.html', party_prediction = party_result)
 
 @app.route('/sitemap')
