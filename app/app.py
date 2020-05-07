1411d6ca3eef39d15eeb67e9ce9d331890d81b71
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-05-06 21:19:56 -0500

    debugging

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, redirect, make_response, json, request
 import os
 from joblib import load
+import subprocess
 
 # Tools to remove stopwords from tweets
 import nltk
@@ -14,12 +15,12 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 stop_words = set(stopwords.words('english'))
 
-# # #temp_path = os.path.abspath('static/js/data/data_preprocessors/bigram_vectorizer.joblib')
-# bigram_vectorizer = load('/app/static/js/data/data_preprocessors/bigram_vectorizer.joblib')
-# # #temp_path = os.path.abspath('./static/js/data/data_preprocessors/bigram_tf_idf_transformer.joblib')
-# bigram_tf_idf_transformer = load('/app/static/js/data/data_preprocessors/bigram_tf_idf_transformer.joblib')
-# # #temp_path = os.path.abspath('./static/js/data/classifiers/sgd_classifier.joblib')
-# sgd_classifier = load('/app/static/js/data/classifiers/sgd_classifier.joblib')
+# #temp_path = os.path.abspath('static/js/data/data_preprocessors/bigram_vectorizer.joblib')
+bigram_vectorizer = load('static/js/data/data_preprocessors/bigram_vectorizer.joblib')
+# #temp_path = os.path.abspath('./static/js/data/data_preprocessors/bigram_tf_idf_transformer.joblib')
+bigram_tf_idf_transformer = load('static/js/data/data_preprocessors/bigram_tf_idf_transformer.joblib')
+# #temp_path = os.path.abspath('./static/js/data/classifiers/sgd_classifier.joblib')
+sgd_classifier = load('static/js/data/classifiers/sgd_classifier.joblib')
 
 app = Flask(__name__)
 app.config['DEBUG']= True
@@ -47,23 +48,20 @@ def my_form():
 
 @app.route('/mlmodels', methods=['POST'])
 def mlmodels():
-    # in_text = request.form['text']
-    # def list_tostring(input_list):
-    #     return ' '.join(input_list)
-    # def remove_stopwords(input_list):
-    #     return [w for w in input_list if not w in stop_words]
-    # fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
+    in_text = request.form['text']
+    def list_tostring(input_list):
+        return ' '.join(input_list)
+    def remove_stopwords(input_list):
+        return [w for w in input_list if not w in stop_words]
+    fun_input = list_tostring(remove_stopwords(word_tokenize(in_text)))
 
-    # X_pred = bigram_vectorizer.transform([fun_input])
-    # X_pred = bigram_tf_idf_transformer.transform(X_pred)
-    # result = sgd_classifier.predict(X_pred)
-    result = ['no good']
+    X_pred = bigram_vectorizer.transform([fun_input])
+    X_pred = bigram_tf_idf_transformer.transform(X_pred)
+    result = sgd_classifier.predict(X_pred)
     if result[0] == 'R':
         party_result = 'Predicted Republican Tweet'
     else:
-        path_text = os.getcwd()
-        ls_text = os.system('ls')
-        party_result = 'Predicted Democrat Tweet  '+path_text+' '+ls_text
+        party_result = 'Predicted Democrat Tweet  '
     return render_template('mlmodels.html', party_prediction = party_result)
 
 @app.route('/sitemap')
