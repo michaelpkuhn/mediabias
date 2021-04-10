8994d934acb55e7ed5f2f3e7802884868b5284c6
michaelpkuhn <mickuhn95@gmail.com>
2021-04-10 12:45:16 -0500

    moved export path to yml

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
 
-os.environ['PYTHONPATH'] = "/d/misc_projects_d/mediabias"
 os.system('conda install --file project_env.txt')
 os.system('pip install gunicorn==20.0.4 psycopg2-binary==2.8.4 pytest==6.2.3')
