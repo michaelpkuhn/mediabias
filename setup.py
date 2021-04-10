e02d1eb78accd2ad344abd91e40890bca764bea3
michaelpkuhn <mickuhn95@gmail.com>
2021-04-10 12:36:02 -0500

    added python path env variable to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
 
+os.environ['PYTHONPATH'] = "/d/misc_projects_d/mediabias"
 os.system('conda install --file project_env.txt')
 os.system('pip install gunicorn==20.0.4 psycopg2-binary==2.8.4 pytest==6.2.3')
